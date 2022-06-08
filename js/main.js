13d5d6916829b2dd620e75d2132aabd908cba691
Olga Marinina <youarewonderful@yandex.ru>
2022-06-08 20:23:32 +0700

    Доработала функцию вывода рандомного числа

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 /**
  * Get random ceil number between min and max
  * Use reference - https://www.schoolsw3.com/js/js_random.php
- * @param min min number of gap
- * @param max max number of gap
+ * @param fromInt min number of gap
+ * @param toInt max number of gap
  * @return random number between this gap
 */
-const getRndInteger = function (min, max) {
-  if (Math.sign(min) === -1 || Math.sign(max) === -1) {
-    return 'Диапазон может быть только положительный, включая ноль';
-  }
-  if (min >= max) {
-    return 'Максимальное число диапазона должно быть больше минимального';
-  }
+const getRndInteger = function (fromNum, toNum) {
+  const min = Math.ceil(Math.min(fromNum, toNum));
+  const max = Math.floor(Math.max(fromNum, toNum));
+
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 };
 
