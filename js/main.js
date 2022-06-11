55d13725bde2cee35a0f8079a4ae0d471bfe289a
Olga Marinina <youarewonderful@yandex.ru>
2022-06-11 22:28:23 +0700

    Сделала другой формат генерации описания фотографий

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
  * Use reference - https://www.schoolsw3.com/js/js_random.php
  * @param fromInt min number of gap
  * @param toInt max number of gap
- * @return random number between this gap
+ * @returns random number between this gap
 */
 const getRndInteger = function (fromNum, toNum) {
   const min = Math.ceil(Math.min(fromNum, toNum));
@@ -18,7 +18,7 @@ getRndInteger(0, 5);
  * Check that a string is no longer than the max length
  * @param checkString The string which us needed to check
  * @param maxLength The max length of string
- * @return true if length is right, false if not
+ * @returns true if length is right, false if not
  */
 const isRightLength = function (checkString, maxLength) {
   return checkString.length <= maxLength;
@@ -38,7 +38,7 @@ const DESCRIPTIONS = [
   'Всегда знал, что чёрный мне к лицу.',
   'Вклад в будущее, раз Тинькофф не разрешает.',
   'Как неожиданно и приятно. Очень приятно!'
-]
+];
 
 const MESSAGES = [
   'Всё отлично!',
@@ -66,7 +66,7 @@ const USER_NAMES = [
 /**
  * Get unique number from shuffle limit array
  * @param poolSize max number in created array
- * @return unique number from limit array
+ * @returns unique number from limit array
  */
 const getIndex = function (poolSize) {
   const newArray = Array.from({length: poolSize}, (_, i) => i + 1);
@@ -84,57 +84,43 @@ const newPhotoIds = getIndex(NUMBER_ID_PHOTO);
 const newCommentIds = getIndex(NUMBER_ID_COMMENT);
 
 /**
- * Create the random string from elements
- * @param length of new string for random description
- * @return random string
+ * Create text from array random join 1 or 2 sentences
+ * @param array of sentences to create the text
+ * @returns prepared message
  */
-const makeDescription = function (length) {
-  let result = '';
-  const words = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const wordsLength = words.length;
-  for (let i = 0; i < length; i++ ) {
-    result += words.charAt(Math.floor(Math.random() * wordsLength));
-  }
-  return result;
-};
-
-/**
- * Create messege for comments from array random join 1 or 2 sentences
- * @return prepared message
- */
-const createMessage = function () {
-  const messages = [];
+const createText = function (array) {
+  const texts = [];
   const countSentences = getRndInteger(1, 2);
   for (let i = 0; i < countSentences; i++) {
-    messages[i] = [MESSAGES[getRndInteger(0, MESSAGES.length - 1)]];
+    texts[i] = [array[getRndInteger(0, MESSAGES.length - 1)]];
   }
-  const message = messages.join(' ');
-  return message;
+  const text = texts.join(' ');
+  return text;
 };
 
 /**
  * Create one of comments for photo
- * @return object of comment
+ * @returns object of comment
  */
 const comment = function () {
   return {
     idComment: newCommentIds.pop(),
     avatar: `img/avatar-${getRndInteger(1, 6)}`,
-    message: createMessage(),
+    message: createText(MESSAGES),
     nameUser: USER_NAMES[getRndInteger(0, USER_NAMES.length - 1)]
   };
 };
 
 /**
  * Create one of photos for post
- * @return object of photos
+ * @returns object of photos
  */
 const dataPhoto = function () {
   const newId = newPhotoIds.pop();
   return {
     id: newId,
     url: `photos/${newId}.jpg`,
-    description: makeDescription(20),
+    description: createText(DESCRIPTIONS),
     likes: getRndInteger(15, 200),
     comments: Array.from({length: getRndInteger(1, 6)}, comment)
   };
