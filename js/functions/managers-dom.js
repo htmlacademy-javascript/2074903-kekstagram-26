3738f58650851f4c1fe621e1e651ac181e1006e0
Olga Marinina <youarewonderful@yandex.ru>
2022-06-21 08:04:59 +0700

    Добавила описание функции

--- js/functions/managers-dom.js
+++ js/functions/managers-dom.js
@@ -1,3 +1,8 @@
+/**
+ * Delete all children of a DOM object from started index
+ * @param { object } parentElement psrent element including needed children
+ * @param { int } startedIndex index from which we start to delete children
+ */
 const removeAllAddedChildren = (parentElement, startedIndex) => {
   const addedElements = parentElement.children;
   while (addedElements[startedIndex]) {
