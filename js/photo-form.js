2e095ee12c7d2a8425037fda9a268acfe485816e
Olga Marinina <youarewonderful@yandex.ru>
2022-06-23 22:02:34 +0700

    Подготовила функции для валидации хэштегов

--- js/photo-form.js
+++ js/photo-form.js
@@ -47,3 +47,42 @@ fieldUploadPhoto.addEventListener('change', () => {
   //addedPhotoPreview.src = fieldUploadPhoto.value;
   exitForm();
 });
+
+const regexCheckHashtag = /^[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+
+const validateHashtag = (value) => {
+  const hashtags = value.split(' ');
+  hashtags.forEach((hashtag) => {
+    if (!regexCheckHashtag.test(hashtag)) {
+      return false;
+    }
+  });
+  for (let i = 0; i < hashtags.length; i++) {
+    for (let j = i + 1; j < hashtags.length; j++) {
+      if (hashtags[i].toLowerCase() === hashtags[j].toLowerCase()) {
+        return false;
+      }
+    }
+  }
+  return hashtags.length <= 5;
+};
+
+const getHashtagErrorMessage = (value) => {
+  const hashtags = value.split(' ');
+  hashtags.forEach((hashtag) => {
+    if (!regexCheckHashtag.test(hashtag)) {
+      return 'Cтрока после решётки должна состоять из букв и чисел' +
+      ' и не может содержать пробелы, спецсимволы (#, @, $ и т. п.),' +
+      ' символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.';
+    }
+  });
+  for (let i = 0; i < hashtags.length; i++) {
+    for (let j = i + 1; j < hashtags.length; j++) {
+      if (hashtags[i].toLowerCase() === hashtags[j].toLowerCase()) {
+        return 'Ваши хэш-теги повторяются, проверьте: хэш-теги нечувствительны к регистру:' +
+        ' #ХэшТег и #хэштег считаются одним и тем же тегом';
+      }
+    }
+  }
+  return hashtags.length <= 5 ? null : 'Нельзя указать больше пяти хэш-тегов';
+};
