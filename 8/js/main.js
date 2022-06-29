d6a812a566303bb292260268a65f14ee1785335e
Keks <keks@htmlacademy.ru>
2022-06-29 12:59:27 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,11 @@
+import { createDataPhotos } from './mocks/generators.js';
+import { NUMBER_PHOTO } from './mocks/generation.js';
+import { addPreviews } from './photo-renderer.js';
+import { addOpenFullPhotoHandler } from './full-photo.js';
+import { addOpenFormUploadPhotoHandler, addPristineValidatorsFromFields } from './photo-form.js';
+
+const dataPhotos = createDataPhotos(NUMBER_PHOTO);
+addPreviews(dataPhotos);
+addOpenFullPhotoHandler(dataPhotos);
+addOpenFormUploadPhotoHandler();
+addPristineValidatorsFromFields();
