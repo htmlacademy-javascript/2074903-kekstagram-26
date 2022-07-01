9cccfa8dba74da4f479e177f09674cac6b3252a7
Olga Marinina <youarewonderful@yandex.ru>
2022-07-01 19:55:38 +0700

    Перенесла не константы в соответствующий модуль

--- js/constants.js
+++ js/constants.js
@@ -10,96 +10,6 @@ const SCALE_PHOTO_MIN = 25;
 const SCALE_PHOTO_MAX = 100;
 const SCALE_PHOTO_CHANGE = 25;
 
-const formatValueEffect = {
-  to: function (value) {
-    if (Number.isInteger(value)) {
-      return value.toFixed(0);
-    }
-    return value.toFixed(1);
-  },
-  from: function (value) {
-    return parseFloat(value);
-  },
-};
-
-const effects = {
-  'chrome': {
-    filter: 'grayscale',
-    class: 'effects__preview--chrome',
-    typeValue: '',
-    slider: {
-      range: {
-        min: 0,
-        max: 1,
-      },
-      start: 1,
-      step: 0.1,
-      connect: 'lower',
-      format: formatValueEffect,
-    }
-  },
-  'sepia': {
-    filter: 'sepia',
-    class: 'effects__preview--sepia',
-    typeValue: '',
-    slider: {
-      range: {
-        min: 0,
-        max: 1,
-      },
-      start: 1,
-      step: 0.1,
-      connect: 'lower',
-      format: formatValueEffect,
-    }
-  },
-  'marvin': {
-    filter: 'invert',
-    class: 'effects__preview--marvin',
-    typeValue: '%',
-    slider: {
-      range: {
-        min: 0,
-        max: 100,
-      },
-      start: 100,
-      step: 1,
-      connect: 'lower',
-      format: formatValueEffect,
-    }
-  },
-  'phobos': {
-    filter: 'blur',
-    class: 'effects__preview--phobos',
-    typeValue: 'px',
-    slider: {
-      range: {
-        min: 0,
-        max: 3,
-      },
-      start: 3,
-      step: 0.1,
-      connect: 'lower',
-      format: formatValueEffect,
-    }
-  },
-  'heat': {
-    filter: 'brightness',
-    class: 'effects__preview--heat',
-    typeValue: '',
-    slider: {
-      range: {
-        min: 1,
-        max: 3,
-      },
-      start: 3,
-      step: 0.1,
-      connect: 'lower',
-      format: formatValueEffect,
-    }
-  }
-};
-
 export {
   LENGTH_COMMENT,
   COUNT_HASHTAGS,
@@ -108,6 +18,5 @@ export {
   COUNT_COMMENTS_PER_PAGE,
   SCALE_PHOTO_MIN,
   SCALE_PHOTO_MAX,
-  SCALE_PHOTO_CHANGE,
-  effects
+  SCALE_PHOTO_CHANGE
 };
--- js/upload-form/photo-effects.js
+++ js/upload-form/photo-effects.js
@@ -1,11 +1,99 @@
-import { effects } from '../constants.js';
-
 const uploadPhotoForm = document.querySelector('.img-upload__form');
 const sliderContainer = uploadPhotoForm.querySelector('.img-upload__effect-level');
 const sliderEffects = uploadPhotoForm.querySelector('.effect-level__slider');
 const valueEffect = uploadPhotoForm.querySelector('.effect-level__value');
 const effectsContainer = uploadPhotoForm.querySelector('.effects__list');
 const previewPhotoForm = uploadPhotoForm.querySelector('.img-upload__preview img');
+
+const formatValueEffect = {
+  to: function (value) {
+    if (Number.isInteger(value)) {
+      return value.toFixed(0);
+    }
+    return value.toFixed(1);
+  },
+  from: function (value) {
+    return parseFloat(value);
+  },
+};
+
+const effects = {
+  'chrome': {
+    filter: 'grayscale',
+    class: 'effects__preview--chrome',
+    typeValue: '',
+    slider: {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 1,
+      step: 0.1,
+      connect: 'lower',
+      format: formatValueEffect,
+    }
+  },
+  'sepia': {
+    filter: 'sepia',
+    class: 'effects__preview--sepia',
+    typeValue: '',
+    slider: {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 1,
+      step: 0.1,
+      connect: 'lower',
+      format: formatValueEffect,
+    }
+  },
+  'marvin': {
+    filter: 'invert',
+    class: 'effects__preview--marvin',
+    typeValue: '%',
+    slider: {
+      range: {
+        min: 0,
+        max: 100,
+      },
+      start: 100,
+      step: 1,
+      connect: 'lower',
+      format: formatValueEffect,
+    }
+  },
+  'phobos': {
+    filter: 'blur',
+    class: 'effects__preview--phobos',
+    typeValue: 'px',
+    slider: {
+      range: {
+        min: 0,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1,
+      connect: 'lower',
+      format: formatValueEffect,
+    }
+  },
+  'heat': {
+    filter: 'brightness',
+    class: 'effects__preview--heat',
+    typeValue: '',
+    slider: {
+      range: {
+        min: 1,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1,
+      connect: 'lower',
+      format: formatValueEffect,
+    }
+  }
+};
 
 const addPhotoEffect = (selectedEffect) => {
   const effect = effects[selectedEffect.value];
